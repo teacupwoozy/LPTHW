e9c0d727dd625ee553fa6097dc1449c5aa111739
Stacy Montemayor <stacy.montemayor@gmail.com>
2018-11-09 14:38:24 -0600

    initial commit, outline classes

---
+++ game_ex45.py
@@ -0,0 +1,46 @@
+from sys import exit
+
+class Scene(object):
+
+    def enter(self):
+        pass
+
+
+class Engine(object):
+
+    def __init__(self, scene_map):
+        pass
+    
+    def play(self)
+        pass
+
+
+class RacistsRoom(Scene):
+
+    def enter(self):
+        pass
+
+class KittensPuppies(Scene):
+
+    def enter(self):
+        pass
+
+class MisogynistMeeting(Scene):
+
+    def enter(self):
+        pass
+
+class CollaborationCenter(Scene):
+
+    def enter(self):
+        pass
+
+class WashingtonDC(Scene):
+
+    def enter(self):
+        pass
+
+class NationalParks(Scene):
+
+    def enter(self):
+        pass
