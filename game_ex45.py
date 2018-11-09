b9f709ecf0eb8e0922fc739e348f1fa2ae124331
Stacy Montemayor <stacy.montemayor@gmail.com>
2018-11-09 14:55:16 -0600

    add all remaining classes

--- game_ex45.py
+++ game_ex45.py
@@ -11,7 +11,7 @@ class Engine(object):
     def __init__(self, scene_map):
         pass
     
-    def play(self)
+    def play(self):
         pass
 
 
@@ -20,27 +20,85 @@ class RacistsRoom(Scene):
     def enter(self):
         pass
 
+
 class KittensPuppies(Scene):
 
     def enter(self):
         pass
+
 
 class MisogynistMeeting(Scene):
 
     def enter(self):
         pass
 
+
 class CollaborationCenter(Scene):
 
     def enter(self):
         pass
+
 
 class WashingtonDC(Scene):
 
     def enter(self):
         pass
 
+
 class NationalParks(Scene):
 
     def enter(self):
         pass
+
+
+class Recharge(Scene):
+
+    def enter(self):
+        pass
+
+
+class ConvertEvilOnes(Scene):
+
+    def enter(self):
+        pass
+
+    def read_book(self):
+        pass
+
+    def personal_connection(self):
+        pass
+
+
+class BeAnAlly(Scene):
+
+    def enter(self):
+        pass
+
+
+class EmpathyMagicSpell(Scene):
+
+    def enter(self):
+        pass
+
+class SetBack(Scene):
+
+    def enter(self):
+        pass
+    
+    def defeat(self):
+        pass
+    
+    def reassess(self):
+        pass
+    
+    def evil_ones_elected(self):
+        pass
+    
+    def natural_resources_selloff(self):
+        pass
+
+class AlliesElected(Scene):
+
+    def enter(self):
+        pass
+
